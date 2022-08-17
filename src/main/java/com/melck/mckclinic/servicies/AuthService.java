f7e5a148211992d89c0c88d973288907e20a9aa1
melck <melck_junior@hotmail.com>
2022-08-17 10:32:40 +0100

    Auth service

---
+++ src/main/java/com/melck/mckclinic/servicies/AuthService.java
@@ -0,0 +1,35 @@
+package com.melck.mckclinic.servicies;
+
+import com.melck.mckclinic.entities.User;
+import com.melck.mckclinic.repositories.UserRepository;
+import com.melck.mckclinic.servicies.exceptions.ForbiddenException;
+import com.melck.mckclinic.servicies.exceptions.UnauthorizedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Transactional(readOnly = true)
+    public User authenticated() {
+        try {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            return repository.findByCpf(username);
+        } catch (Exception e){
+            throw new UnauthorizedException("Invalid user");
+        }
+    }
+
+    public void validateSelfOrAdmin(Long userId){
+        var user = authenticated();
+
+        if(!user.getId().equals(repository.findById(userId)) && !user.hasRole("ROLE_ADMIN")){
+            throw new ForbiddenException("Access denied");
+        }
+    }
+}
